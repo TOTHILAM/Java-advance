b5a70ddde249a8ccce44f6f5e428016abff4d700
TOTHILAM <tolam0902@gmail.com>
2024-09-01 15:14:28 +0900

    create

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Program {
+import entity.Department;
+import util.HibernateUtil;
+
+public class Program {
+    public static void main(String[] args) {
+        try (var factory = HibernateUtil.buidSessionFactory()) {
+            factory.inTransaction(session -> {
+                var department = new Department();
+                department.setName("Giám đốc");
+                session.persist(department);
+            });
+            factory.inTransaction(session -> {
+                var department = new Department();
+                department.setName("Bảo vệ");
+                session.persist(department);
+            });
+
+            factory.inSession(session -> {
+                var departments = session.createSelectionQuery("FROM Department", Department.class)
+                        .getResultList();
+                for (Department department : departments) {
+                    System.out.println("◉　department = " + department);
+                }
+            });
+
+            factory.inSession(session -> {
+                var department = session.get(Department.class, 1);
+                System.out.println("✨ department = " + department);
+            });
+
+            factory.inSession(session -> {
+                var hql = "FROM Department WHERE name = :name";
+                var department = session
+                        .createSelectionQuery(hql, Department.class)
+                        .setParameter("name", "Bảo vệ")
+                        .uniqueResult();
+                System.out.println("🍎 department = " + department);
+            });
+
+            factory.inTransaction(session -> {
+                var department = session.get(Department.class, 2);
+                department.setName("Kinh doanh");
+                session.merge(department);
+            });
+
+            factory.inTransaction(session -> {
+                var department = session.get(Department.class, 1);
+                session.remove(department);
+            });
+        }
+    }
 }
--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,2 +1,21 @@
-package ultil;public class HibernateUtil {
+package util;
+
+import entity.Department;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    public static SessionFactory buidSessionFactory() {
+        var url = "jdbc:mysql://localhost:3306/lesson_01?createDatabaseIfNotExist=true";
+        var configuration = new Configuration()
+                .addAnnotatedClass(Department.class)
+                .setProperty(AvailableSettings.URL, url)
+                .setProperty(AvailableSettings.USER, "root")
+                .setProperty(AvailableSettings.PASS, "Ritto2024")
+                .setProperty(AvailableSettings.HBM2DDL_AUTO, "create")
+                .setProperty(AvailableSettings.SHOW_SQL, "true")
+                .setProperty(AvailableSettings.HIGHLIGHT_SQL, "true");
+        return configuration.buildSessionFactory();
+    }
 }
