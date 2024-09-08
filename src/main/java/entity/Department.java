741fe4e907af8bb3ca75dbb46fc6717a7c901553
TOTHILAM <tolam0902@gmail.com>
2024-09-08 10:24:58 +0900

    create

--- src/main/java/entity/Department.java
+++ src/main/java/entity/Department.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.type.descriptor.jdbc.CharJdbcType;
 
@@ -21,9 +22,13 @@ import java.util.UUID;
 public class Department {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @JdbcType(CharJdbcType.class)
-    private UUID id;
+
+    @GenericGenerator(
+            name = "department_id_generator",
+            strategy = "generator.DepartmentIdGenarator"
+    )
+    @GeneratedValue(generator = "department_id_generator")
+    private String id;
 
     @Column(name = "name", length = 50, unique = true, nullable = false)
     private String name;
