8abead38f8872e6240946f8c9761c97883851390
TOTHILAM <tolam0902@gmail.com>
2024-09-08 13:50:36 +0900

    create

--- src/main/java/entity/GroupAccount.java
+++ src/main/java/entity/GroupAccount.java
@@ -15,20 +15,31 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "group_account")
 public class GroupAccount {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @EmbeddedId
+    private PrimaryKey pk;
 
     @ManyToOne
     @JoinColumn(name = "group_id", referencedColumnName = "id", nullable = false)
+    @MapsId("group_id")
     private Group group;
 
     @ManyToOne
     @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false)
+    @MapsId("account_id")
     private Account account;
 
     @Column(name = "joined_at", nullable = false, updatable = false)
     @CreationTimestamp
     private LocalDateTime joinedAt;
+
+    @Setter
+    @Getter
+    @Embeddable
+    public static class  PrimaryKey implements Serializable{
+        @Column(name = "group_id", nullable = false)
+        private int groupId;
+
+        @Column(name = "account_id", nullable = false)
+        private int accountId;
+    }
 }
