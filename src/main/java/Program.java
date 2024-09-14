986d23638de46c7cd4f0a9f493f7c6a6ab161816
TOTHILAM <tolam0902@gmail.com>
2024-09-14 14:59:04 +0900

    create

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -8,11 +8,13 @@ public class Program {
         try (var factory = HibernateUtil.buidSessionFactory()) {
             factory.inTransaction(session -> {
                 var circle = new Circle();
+                circle.setId(1);
                 circle.setColor("red");
                 circle.setRadius(5);
                 session.persist(circle);
 
                 var rectangle = new Rectangle();
+                rectangle.setId(2);
                 rectangle.setColor("blue");
                 rectangle.setWidth(3);
                 rectangle.setHeight(4);
@@ -20,12 +22,13 @@ public class Program {
             });
 
             factory.inSession(session -> {
-                var hql = "FROM Shape";
+                var hql = "FROM Circle";
                 var shapes = session
-                        .createSelectionQuery(hql, Shape.class)
+                        .createSelectionQuery(hql, Circle.class)
                         .getResultList();
                 for (Shape shape : shapes) {
                     System.out.println("shape = " + shape.getColor());
+                    System.out.println("shape = " + shape.getId());
                 }
             });
         }
